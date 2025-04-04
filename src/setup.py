b96cc186b7cd22121075360fc01c9c2e5961304d
daw8806@yahoo.com <daw8806@yahoo.com>
2025-04-03 22:06:00 -0500

    Now creates cut out of just the eye and color shifts it for easier analysis

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,7 @@
 def eyeDetection():
     import cv2 as cv
     import dlib
+    import numpy as np
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
@@ -19,12 +20,27 @@ def eyeDetection():
             cv.rectangle(frame, (x,y), (w,h),color, thickness)
 
             landmarks= predictor(gray,face)
-
-            rightEye_midpoint = int((landmarks.part(43).x + landmarks.part(46).x)/2), int((landmarks.part(43).y + landmarks.part(46).y)/2)
-            leftEye_midpoint = int((landmarks.part(36).x + landmarks.part(39).x)/2), int((landmarks.part(36).y + landmarks.part(39).y)/2)
-            cv.circle(frame, leftEye_midpoint, 1, color, thickness) 
-            cv.circle(frame, rightEye_midpoint, 1, color, thickness)
-        cv.imshow('Camera',frame)
+            height,width, _= frame.shape
+            mask = np.zeros((height,width), np.uint8)
+            leftEyeRegion = np.array([(landmarks.part(36).x,landmarks.part(36).y),
+                                      (landmarks.part(37).x,landmarks.part(37).y),
+                                      (landmarks.part(38).x,landmarks.part(38).y),
+                                      (landmarks.part(39).x,landmarks.part(39).y),
+                                      (landmarks.part(40).x,landmarks.part(40).y),
+                                      (landmarks.part(41).x,landmarks.part(41).y)], np.int32)
+            cv.polylines(mask, [leftEyeRegion], True, 255, 2)
+            cv.fillPoly(mask, [leftEyeRegion], 255)
+            left_eye = cv.bitwise_and(gray, gray, mask=mask)
+            min_x = np.min(leftEyeRegion[:, 0])
+            max_x = np.max(leftEyeRegion[:, 0])
+            min_y = np.min(leftEyeRegion[:, 1])
+            max_y = np.max(leftEyeRegion[:, 1])
+            gray_eye = left_eye[min_y: max_y, min_x: max_x]
+            _, threshold = cv.threshold(gray_eye, 70, 255, cv.THRESH_BINARY)
+            threshold = cv.resize(threshold, None, fx=5, fy=5)
+            eye = cv.resize(gray_eye, None, fx=5, fy=5)
+            cv.imshow("Eye",left_eye)
+ #       cv.imshow('Camera',frame)
 
         if cv.waitKey(1) == ord('q'):
             break
